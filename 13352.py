597e949d2f3cae16de88ab39fbacdfff3d8e17d7
shreyash4755 <shreyashtilekar4755@gmail.com>
2025-01-22 13:30:00 +0530

    Add files via upload

---
+++ 13352.py
@@ -0,0 +1,34 @@
+#Q1
+roll_no=101
+name="Rajesh"
+print("Roll no is",roll_no,"Name of student is",name)
+
+#Q2
+x=['a','x','y','z',['d','e'],'f','p','q','r']
+x.append("g")
+print(x)
+
+x[4].insert(2,"n")
+print(x)
+
+#Q3
+student={"Rollno":101, "Name":"Ajay"}
+student["Fees"]=4000
+print(student)
+
+#Q4
+a={2,3,4,5}
+a.add(7)
+a.add(8)
+print(a)
+
+#Q5
+x={2,3,4,5}
+
+y=list(x)
+y.append(7)
+
+x=tuple(y)
+print(x)
+
+
